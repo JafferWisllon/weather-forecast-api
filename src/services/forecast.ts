40cba56dba2ae6fd75e5722fa41fab8af2534fd1
Jaffer Wisllon <contatojaffer@gmail.com>
2022-01-08 11:35:42 -0300

    feat: integrate Beach model, forecast service with api

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -14,7 +14,6 @@ export interface Beach {
   position: BeachPosition;
   lat: number;
   lng: number;
-  user: string;
 }
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
