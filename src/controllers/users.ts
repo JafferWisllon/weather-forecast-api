3aea2b52985bb6ef2287996c30d2d89d358c6cfc
Jaffer Wisllon <contatojaffer@gmail.com>
2022-01-11 20:53:32 -0300

    feat: ensure UserController return 422 when there is a validation error

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,13 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Controller, Post } from '@overnightjs/core';
 import { User } from '@src/models/user';
 import { Request, Response } from 'express';
+import { BaseController } from '.';
 
 @Controller('users')
-export class UsersController {
+export class UsersController extends BaseController {
   @Post('')
   public async getForecastForLoggedUser(req: Request, res: Response): Promise<void> {
-    const user = new User(req.body);
-    const newUser = await user.save()
-    res.status(201).send(newUser);   
+    try {
+      const user = new User(req.body);
+      const newUser = await user.save()
+      res.status(201).send(newUser); 
+    }catch(error: any) {
+      this.sendCreatedUpdateErrorResponse(res, error)
+    }
   }
 }
