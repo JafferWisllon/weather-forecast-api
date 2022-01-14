5927947f99298e62237110a971134cd1568e02ef
Jaffer Wisllon <contatojaffer@gmail.com>
2022-01-14 20:13:41 -0300

    feat: ensure auth route returns 401 if user not found by given email

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -65,5 +65,12 @@ describe('Users functional tests', () => {
         expect.objectContaining({ token: expect.any(String)})
       )
     });
+    it('should return UNAUTHORIZED if the user with the given email is not found', async () => {
+      const response = await global.testRequest
+      .post('/users/authenticate')
+      .send({ email: 'some-email@mail.com', password: '1234' });
+      
+    expect(response.status).toBe(401);
+    });
   });  
 });
